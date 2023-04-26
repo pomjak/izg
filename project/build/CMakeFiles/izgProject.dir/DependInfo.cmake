5bf606ac03216777163976bfd22fa5ae60c17523
pomjak <pomjak71@gmail.com>
2023-04-26 18:19:26 +0200

    adding build

---
+++ project/build/CMakeFiles/izgProject.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/angry.cpp" "CMakeFiles/izgProject.dir/examples/angry.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/angry.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/czFlagMethod.cpp" "CMakeFiles/izgProject.dir/examples/czFlagMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/czFlagMethod.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/emptyMethod.cpp" "CMakeFiles/izgProject.dir/examples/emptyMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/emptyMethod.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/modelMethod.cpp" "CMakeFiles/izgProject.dir/examples/modelMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/modelMethod.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/phongMethod.cpp" "CMakeFiles/izgProject.dir/examples/phongMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/phongMethod.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/rotatingTriangles.cpp" "CMakeFiles/izgProject.dir/examples/rotatingTriangles.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/rotatingTriangles.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/skFlagMethod.cpp" "CMakeFiles/izgProject.dir/examples/skFlagMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/skFlagMethod.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/stairs.cpp" "CMakeFiles/izgProject.dir/examples/stairs.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/stairs.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/texturedQuadMethod.cpp" "CMakeFiles/izgProject.dir/examples/texturedQuadMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/texturedQuadMethod.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/triangle3DMethod.cpp" "CMakeFiles/izgProject.dir/examples/triangle3DMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/triangle3DMethod.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/triangleBufferMethod.cpp" "CMakeFiles/izgProject.dir/examples/triangleBufferMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/triangleBufferMethod.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/triangleClip1Method.cpp" "CMakeFiles/izgProject.dir/examples/triangleClip1Method.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/triangleClip1Method.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/triangleClip2Method.cpp" "CMakeFiles/izgProject.dir/examples/triangleClip2Method.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/triangleClip2Method.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/examples/triangleMethod.cpp" "CMakeFiles/izgProject.dir/examples/triangleMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/triangleMethod.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/framework/application.cpp" "CMakeFiles/izgProject.dir/framework/application.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/application.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/framework/arguments.cpp" "CMakeFiles/izgProject.dir/framework/arguments.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/arguments.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/framework/bunny.cpp" "CMakeFiles/izgProject.dir/framework/bunny.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/bunny.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/framework/main.cpp" "CMakeFiles/izgProject.dir/framework/main.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/main.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/framework/model.cpp" "CMakeFiles/izgProject.dir/framework/model.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/model.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/framework/programContext.cpp" "CMakeFiles/izgProject.dir/framework/programContext.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/programContext.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/framework/systemSpecific.cpp" "CMakeFiles/izgProject.dir/framework/systemSpecific.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/systemSpecific.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/framework/textureData.cpp" "CMakeFiles/izgProject.dir/framework/textureData.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/textureData.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/framework/window.cpp" "CMakeFiles/izgProject.dir/framework/window.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/window.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/libs/impl.cpp" "CMakeFiles/izgProject.dir/libs/impl.cpp.o" "gcc" "CMakeFiles/izgProject.dir/libs/impl.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/student/drawModel.cpp" "CMakeFiles/izgProject.dir/student/drawModel.cpp.o" "gcc" "CMakeFiles/izgProject.dir/student/drawModel.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/student/gpu.cpp" "CMakeFiles/izgProject.dir/student/gpu.cpp.o" "gcc" "CMakeFiles/izgProject.dir/student/gpu.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/clippingTests.cpp" "CMakeFiles/izgProject.dir/tests/clippingTests.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/clippingTests.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/commandBufferToStr.cpp" "CMakeFiles/izgProject.dir/tests/commandBufferToStr.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/commandBufferToStr.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/commandTests.cpp" "CMakeFiles/izgProject.dir/tests/commandTests.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/commandTests.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/conformanceTests.cpp" "CMakeFiles/izgProject.dir/tests/conformanceTests.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/conformanceTests.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/drawModelTests.cpp" "CMakeFiles/izgProject.dir/tests/drawModelTests.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/drawModelTests.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/finalImageTest.cpp" "CMakeFiles/izgProject.dir/tests/finalImageTest.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/finalImageTest.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/fragmentShaderTests.cpp" "CMakeFiles/izgProject.dir/tests/fragmentShaderTests.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/fragmentShaderTests.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/gpuMemToStr.cpp" "CMakeFiles/izgProject.dir/tests/gpuMemToStr.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/gpuMemToStr.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/modelTestUtils.cpp" "CMakeFiles/izgProject.dir/tests/modelTestUtils.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/modelTestUtils.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/performanceTest.cpp" "CMakeFiles/izgProject.dir/tests/performanceTest.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/performanceTest.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/renderMethodFrame.cpp" "CMakeFiles/izgProject.dir/tests/renderMethodFrame.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/renderMethodFrame.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/saveFrame.cpp" "CMakeFiles/izgProject.dir/tests/saveFrame.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/saveFrame.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/shaderTests.cpp" "CMakeFiles/izgProject.dir/tests/shaderTests.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/shaderTests.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/takeScreenShot.cpp" "CMakeFiles/izgProject.dir/tests/takeScreenShot.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/takeScreenShot.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/testCommon.cpp" "CMakeFiles/izgProject.dir/tests/testCommon.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/testCommon.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/traverseModelTests.cpp" "CMakeFiles/izgProject.dir/tests/traverseModelTests.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/traverseModelTests.cpp.o.d"
+  "/home/pomjak/Dropbox/VUT/4.semestr/IZG/project/tests/vertexShaderTests.cpp" "CMakeFiles/izgProject.dir/tests/vertexShaderTests.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/vertexShaderTests.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project/build/libs/Catch2-3.3.1/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o.d
@@ -0,0 +1,4 @@
+libs/Catch2-3.3.1/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o: \
+ /home/pomjak/Dropbox/VUT/4.semestr/IZG/project/libs/Catch2-3.3.1/src/catch2/internal/catch_result_type.cpp \
+ /usr/include/stdc-predef.h \
+ /home/pomjak/Dropbox/VUT/4.semestr/IZG/project/libs/Catch2-3.3.1/src/catch2/../catch2/internal/catch_result_type.hpp
