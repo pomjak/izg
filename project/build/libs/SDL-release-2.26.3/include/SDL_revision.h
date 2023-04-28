158221dbc72d81d42976a6f2b593328685ccc2ac
pomjak <pomjak71@gmail.com>
2023-04-29 01:18:37 +0200

    startin... test 0 passed

--- project/build/libs/SDL-release-2.26.3/include/SDL_revision.h
+++ project/build/libs/SDL-release-2.26.3/include/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.26.3-g19ae27c (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-2.26.3-g5bf606a (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-2.26.3-g19ae27c"
+#define SDL_REVISION "SDL-2.26.3-g5bf606a"
 #endif
--- project/student/gpu.cpp
+++ project/student/gpu.cpp
@@ -7,37 +7,81 @@
 
 #include <student/gpu.hpp>
 
+void clear(GPUMemory &mem, ClearCommand cmd)
+{
+	if (cmd.clearColor)
+	{
+		for (int i = 0 ; i+4 < (mem.framebuffer.height * mem.framebuffer.width) ; i+=4 )
+		{
+			mem.framebuffer.color[i] = (uint8_t)(cmd.color.r * 255.f);
+			mem.framebuffer.color[i+1] = (uint8_t)(cmd.color.g * 255.f);
+			mem.framebuffer.color[i+2] = (uint8_t)(cmd.color.b * 255.f);
+			mem.framebuffer.color[i+3] = (uint8_t)(cmd.color.a * 255.f);
+		}
+	}
+	if(cmd.clearDepth)
+	{
+		for (int i = 0; i < (mem.framebuffer.height * mem.framebuffer.width); i++)
+		{
+			mem.framebuffer.depth[i] = (cmd.depth);
+		}
+	}
 
-
-//! [gpu_execute]
-void gpu_execute(GPUMemory&mem,CommandBuffer &cb){
-  (void)mem;
-  (void)cb;
-  /// \todo Tato funkce reprezentuje funkcionalitu grafické karty.<br>
-  /// Měla by umět zpracovat command buffer, čistit framebuffer a kresli.<br>
-  /// mem obsahuje paměť grafické karty.
-  /// cb obsahuje command buffer pro zpracování.
-  /// Bližší informace jsou uvedeny na hlavní stránce dokumentace.
-}
-//! [gpu_execute]
-
-/**
- * @brief This function reads color from texture.
- *
- * @param texture texture
- * @param uv uv coordinates
- *
- * @return color 4 floats
- */
-glm::vec4 read_texture(Texture const&texture,glm::vec2 uv){
-  if(!texture.data)return glm::vec4(0.f);
-  auto uv1 = glm::fract(uv);
-  auto uv2 = uv1*glm::vec2(texture.width-1,texture.height-1)+0.5f;
-  auto pix = glm::uvec2(uv2);
-  //auto t   = glm::fract(uv2);
-  glm::vec4 color = glm::vec4(0.f,0.f,0.f,1.f);
-  for(uint32_t c=0;c<texture.channels;++c)
-    color[c] = texture.data[(pix.y*texture.width+pix.x)*texture.channels+c]/255.f;
-  return color;
 }
 
+  //! [gpu_execute]
+	void gpu_execute(GPUMemory & mem, CommandBuffer & cb)
+	{
+		// (void)mem;
+		// (void)cb;
+    	/// todo Tato funkce reprezentuje funkcionalitu grafické karty.<br>
+    	/// Měla by umět zpracovat command buffer, čistit framebuffer a kresli.<br>
+    	/// mem obsahuje paměť grafické karty.
+    	/// cb obsahuje command buffer pro zpracování.
+    	/// Bližší informace jsou uvedeny na hlavní stránce dokumentace.
+		for (uint32_t i = 0; i < cb.nofCommands; ++i)
+		{
+			CommandType type = cb.commands[i].type;
+			CommandData data = cb.commands[i].data;
+
+			if (type == CommandType::CLEAR)
+			{
+				clear(mem, data.clearCommand);
+			}
+			if (type == CommandType::DRAW)
+			{
+				DrawCommand cmd = data.drawCommand;
+				VertexShader vs = mem.programs[cmd.programID].vertexShader;
+				for (uint32_t n = 0; n < cmd.nofVertices; ++n)
+				{
+					InVertex inVertex;
+					OutVertex outVertex;
+					ShaderInterface si;
+					vs(outVertex, inVertex, si);
+				}
+			}
+		}
+	}
+  //! [gpu_execute]
+
+  /**
+   * @brief This function reads color from texture.
+   *
+   * @param texture texture
+   * @param uv uv coordinates
+   *
+   * @return color 4 floats
+   */
+  glm::vec4 read_texture(Texture const &texture, glm::vec2 uv)
+  {
+    if (!texture.data)
+      return glm::vec4(0.f);
+    auto uv1 = glm::fract(uv);
+    auto uv2 = uv1 * glm::vec2(texture.width - 1, texture.height - 1) + 0.5f;
+    auto pix = glm::uvec2(uv2);
+    // auto t   = glm::fract(uv2);
+    glm::vec4 color = glm::vec4(0.f, 0.f, 0.f, 1.f);
+    for (uint32_t c = 0; c < texture.channels; ++c)
+      color[c] = texture.data[(pix.y * texture.width + pix.x) * texture.channels + c] / 255.f;
+    return color;
+  }
