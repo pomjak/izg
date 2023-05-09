4df6adeb31e0d7d826a3f59b5a9d2b7cbefe5b4a
pomjak <pomjak71@gmail.com>
2023-05-09 23:56:34 +0200

    test 20 OK, fuckin buffer clear fixed

--- project/student/gpu.cpp
+++ project/student/gpu.cpp
@@ -11,19 +11,19 @@ void clear(GPUMemory &mem, ClearCommand cmd)
 {
 	if (cmd.clearColor)
 	{
-		for (int i = 0; i + 4 < (mem.framebuffer.height * mem.framebuffer.width); i += 4)
+		for (uint64_t i = 0; i+4 < 4*(mem.framebuffer.height * mem.framebuffer.width); i += 4)
 		{
-			mem.framebuffer.color[i] = (uint8_t)(cmd.color.r * 255.f);
-			mem.framebuffer.color[i + 1] = (uint8_t)(cmd.color.g * 255.f);
-			mem.framebuffer.color[i + 2] = (uint8_t)(cmd.color.b * 255.f);
-			mem.framebuffer.color[i + 3] = (uint8_t)(cmd.color.a * 255.f);
+			mem.framebuffer.color[i] = static_cast<uint8_t>(cmd.color.r * 255.f);
+			mem.framebuffer.color[i + 1] = static_cast<uint8_t>(cmd.color.g * 255.f);
+			mem.framebuffer.color[i + 2] = static_cast<uint8_t>(cmd.color.b * 255.f);
+			mem.framebuffer.color[i + 3] = static_cast<uint8_t>(cmd.color.a * 255.f);
 		}
 	}
 	if (cmd.clearDepth)
 	{
 		for (int i = 0; i < (mem.framebuffer.height * mem.framebuffer.width); i++)
 		{
-			mem.framebuffer.depth[i] = (cmd.depth);
+			mem.framebuffer.depth[i] = cmd.depth;
 		}
 	}
 }
@@ -121,7 +121,6 @@ void TriangleAssembly(GPUMemory &mem, Triangle &triangle, Program const &prg, Ve
 	for (uint32_t i = 0; i < 3; ++i)
 	{
 		InVertex inVertex;
-		OutVertex outVertex;
 
 		uint32_t correctId = i + tId * 3;
 
@@ -197,6 +196,21 @@ void interpolate(InFragment &in, Triangle t, glm::vec3 bar, AttributeType *vs2fs
 		case AttributeType::VEC4:
 			in.attributes[i].v4 = A[i].v4 * perspectiveBary.x + B[i].v4 * perspectiveBary.y + C[i].v4 * perspectiveBary.z;
 			break;
+		case AttributeType::UINT:
+			in.attributes[i].u1 = A[i].u1;
+			break;
+
+		case AttributeType::UVEC2:
+			in.attributes[i].u2 = A[i].u2;
+			break;
+
+		case AttributeType::UVEC3:
+			in.attributes[i].u3 = A[i].u3;
+			break;
+
+		case AttributeType::UVEC4:
+			in.attributes[i].u4 = A[i].u4;
+			break;
 		}
 	}
 }
@@ -215,59 +229,41 @@ void fragmentAssembly(InFragment &in, glm::vec2 pos, Triangle t, AttributeType *
 	interpolate(in, t, bar, vs2fs);
 }
 
-void depthTest(Frame &frame, OutFragment &outF, InFragment &inF, glm::vec2 pos, glm::vec4 &rgb)
+void perFragmentOperations(Frame const &framebuffer, OutFragment &outF, InFragment &inF, const glm::uvec2 &pos)
 {
-	pos -= glm::vec2(0.5f);
+	glm::vec4 color = glm::clamp(outF.gl_FragColor, glm::vec4(0.f), glm::vec4(1.f));
 
-	const uint32_t idx = static_cast<uint32_t>((std::floor(pos.y) * frame.width) + std::floor(pos.x));
+	const uint32_t idx = pos.y * framebuffer.width + pos.x;
 
-	const float inDepth = inF.gl_FragCoord.z;
 	const float alpha = outF.gl_FragColor.w;
+	const float inDepth = inF.gl_FragCoord.z;
 
-	if (frame.depth[idx] > inDepth)
+	if ((alpha < 1.0f) && (framebuffer.depth[idx] >= inDepth))
 	{
-		frame.color[4 * idx] = glm::clamp(static_cast<uint8_t>(rgb.r * 255.f), uint8_t(0), uint8_t(255));
-		frame.color[4 * idx + 1] = glm::clamp(static_cast<uint8_t>(rgb.g * 255.f), uint8_t(0), uint8_t(255));
-		frame.color[4 * idx + 2] = glm::clamp(static_cast<uint8_t>(rgb.b * 255.f), uint8_t(0), uint8_t(255));
-		frame.color[4 * idx + 3] = glm::clamp(static_cast<uint8_t>(rgb.a * 255.f), uint8_t(0), uint8_t(255));
+		color = glm::mix(
+			glm::vec4(static_cast<float>(framebuffer.color[(4 * idx) + 0] / 255.f),
+					  static_cast<float>(framebuffer.color[(4 * idx) + 1] / 255.f),
+					  static_cast<float>(framebuffer.color[(4 * idx) + 2] / 255.f),
+					  static_cast<float>(framebuffer.color[(4 * idx) + 3] / 255.f)),
+			glm::vec4(color.r, color.g, color.b, color.a), alpha);
+		color += glm::vec4(0.0001f);
+
+		color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+	}
+
+	if (framebuffer.depth[idx] >= inDepth)
+	{
 		if (alpha > 0.5f)
-			frame.depth[idx] = inDepth;
+			framebuffer.depth[idx] = inDepth;
+
+		framebuffer.color[(4 * idx) + 0] = static_cast<uint8_t>(color.r * 255.f);
+		framebuffer.color[(4 * idx) + 1] = static_cast<uint8_t>(color.g * 255.f);
+		framebuffer.color[(4 * idx) + 2] = static_cast<uint8_t>(color.b * 255.f);
+		framebuffer.color[(4 * idx) + 3] = static_cast<uint8_t>(color.a * 255.f);
 	}
 }
 
-void blending(Frame &frame, OutFragment &outF, InFragment &inF, glm::vec2 pos, glm::vec4 &rgb)
-{
-	pos -= glm::vec2(0.5f);
-
-	const uint32_t idx = frame.channels * (static_cast<uint32_t>(std::floor(pos.y)) * frame.width + static_cast<uint32_t>(std::floor(pos.x)));
-
-	const float alpha = outF.gl_FragColor.w;
-
-	const glm::vec3 blendedColor = glm::clamp(
-		glm::mix(
-			glm::vec3(frame.color[idx], frame.color[idx + 1], frame.color[idx + 2]) / 255.f,
-			glm::vec3(outF.gl_FragColor[0], outF.gl_FragColor[1], outF.gl_FragColor[2]), alpha),
-		glm::vec3(0.f), glm::vec3(1.f));
-
-	rgb = {blendedColor[0], blendedColor[1], blendedColor[2], 1.f};
-	rgb += 0.0001f;
-}
-
-void perFragmentOperations(Frame &framebuffer, OutFragment &outF, InFragment &inF, const glm::vec2 &pos)
-{
-	outF.gl_FragColor = glm::clamp(outF.gl_FragColor, glm::vec4(0.f), glm::vec4(1.f));
-
-	const glm::vec4 color = outF.gl_FragColor;
-	const float alpha = color.w;
-
-	glm::vec4 blendedColor = color;
-	if (alpha < 1.0f)
-		blending(framebuffer, outF, inF, pos, blendedColor);
-
-	depthTest(framebuffer, outF, inF, pos, blendedColor);
-}
-
-void rasterize(Frame &frame, Triangle const &triangle, Program &prg, ShaderInterface si, bool backFaceCulling)
+void rasterize(Frame const &frame, Triangle const &triangle, Program &prg, ShaderInterface si, bool backFaceCulling)
 {
 	FragmentShader fs = prg.fragmentShader;
 	AttributeType *vs2fs = prg.vs2fs;
@@ -275,9 +271,9 @@ void rasterize(Frame &frame, Triangle const &triangle, Program &prg, ShaderInter
 
 	if (!backFaceCulling)
 	{
-		const float area = (v[1].gl_Position.x - v[0].gl_Position.x) * (v[2].gl_Position.y - v[0].gl_Position.y) -
-						   (v[2].gl_Position.x - v[0].gl_Position.x) * (v[1].gl_Position.y - v[0].gl_Position.y);
-		if (area < 0.0f)
+		const float backface = (v[1].gl_Position.x - v[0].gl_Position.x) * (v[2].gl_Position.y - v[0].gl_Position.y) -
+							   (v[2].gl_Position.x - v[0].gl_Position.x) * (v[1].gl_Position.y - v[0].gl_Position.y);
+		if (backface < 0.0f)
 			std::swap(v[1], v[2]);
 	}
 
@@ -294,11 +290,8 @@ void rasterize(Frame &frame, Triangle const &triangle, Program &prg, ShaderInter
 		min.y = glm::min(min.y, v[i].gl_Position.y);
 	}
 
-	float maxFrameWidth = frame.width;
-	float maxFrameHeight = frame.height;
-
-	max.x = glm::min(max.x, maxFrameWidth - 0.5f);
-	max.y = glm::min(max.y, maxFrameHeight - 0.5f);
+	max.x = glm::min(max.x, static_cast<float>(frame.width - 0.5f));
+	max.y = glm::min(max.y, static_cast<float>(frame.height - 0.5f));
 	min.x = glm::max(min.x, 0.0f);
 	min.y = glm::max(min.y, 0.0f);
 
@@ -309,22 +302,24 @@ void rasterize(Frame &frame, Triangle const &triangle, Program &prg, ShaderInter
 		delta[i] = v[j].gl_Position - v[i].gl_Position;
 	}
 
-	for (uint32_t y = min.y; y <= max.y; ++y)
+	for (uint32_t y = static_cast<uint32_t>(min.y); y <= static_cast<int>(max.y); ++y)
 	{
-		for (uint32_t x = min.x; x <= max.x; ++x)
+		for (uint32_t x = static_cast<uint32_t>(min.x); x <= static_cast<int>(max.x); ++x)
 		{
 			float E[3];
-			glm::vec2 pos{x + 0.5f, y + 0.5f};
+			glm::uvec2 pos{x, y};
+			glm::vec2 pos_f{x + 0.5f, y + 0.5f};
 
 			for (int i = 0; i < 3; i++)
-				E[i] = ((pos.y - v[i].gl_Position.y) * delta[i].x - ((pos.x - v[i].gl_Position.x) * delta[i].y));
+				E[i] = (((pos_f.y - v[i].gl_Position.y) * delta[i].x) - ((pos_f.x - v[i].gl_Position.x) * delta[i].y));
 
 			if (E[0] >= 0.f && E[1] >= 0.f && E[2] >= 0.f)
 			{
 				InFragment inFragment;
-				OutFragment outFragment;
 
-				fragmentAssembly(inFragment, pos, triangle, vs2fs);
+				fragmentAssembly(inFragment, pos_f, triangle, vs2fs);
+
+				OutFragment outFragment;
 
 				fs(outFragment, inFragment, si);
 
@@ -338,8 +333,8 @@ void draw(GPUMemory &mem, DrawCommand cmd, uint32_t draw_id)
 {
 	ShaderInterface si;
 	Program prg = mem.programs[cmd.programID];
-	si.textures = &mem.textures[cmd.programID];
-	si.uniforms = &mem.uniforms[cmd.programID];
+	si.textures = mem.textures;
+	si.uniforms = mem.uniforms;
 
 	for (uint32_t n = 0; n < cmd.nofVertices / 3; ++n)
 	{
